ed62cf61ebb0af814dfaa60ed941a77ce2d79eb7
Nyggen1981 <kjetilnygard81@gmail.com>
2026-01-15 20:40:45 +0100

    Midlertidig: Deaktiver mobil-layout for Ã¥ fikse build

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,15 +4,12 @@ import { SessionProvider } from "next-auth/react"
 import { ReactNode } from "react"
 import { CookieBanner } from "./CookieBanner"
 import { LicenseGuard } from "./LicenseGuard"
-import { ClientLayout } from "./ClientLayout"
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
       <LicenseGuard>
-        <ClientLayout>
-          {children}
-        </ClientLayout>
+        {children}
       </LicenseGuard>
       <CookieBanner />
     </SessionProvider>
