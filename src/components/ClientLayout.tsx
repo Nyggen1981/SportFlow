0eec003c192f6d98534fd2d523cf4bef0ed1389f
Nyggen1981 <kjetilnygard81@gmail.com>
2026-01-15 20:36:14 +0100

    Fix: Bruk full sti til MobileNavbar

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useIsMobile } from "@/hooks/useDevice"
-import { MobileNavbar } from "@/components/mobile"
+import { MobileNavbar } from "@/components/mobile/MobileNavbar"
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react"
 
